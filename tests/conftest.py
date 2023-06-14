788bcc073d3c1768af2898eee2f2c15aa567e6ca
Amanda H. L. de Andrade Katz <amanda.katz@canonical.com>
2023-06-14 09:28:06 -0300

    chore(ISD-782): Add Synapse rock (#5)

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Fixtures for Synapse charm tests."""
+
+from pytest import Parser
+
+
+def pytest_addoption(parser: Parser) -> None:
+    """Parse additional pytest options.
+
+    Args:
+        parser: Pytest parser.
+    """
+    parser.addoption("--synapse-image", action="store")
