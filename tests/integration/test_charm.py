43a8baed53eca6a16a1695ed4ed4add0e42d2b1a
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2023-06-20 17:40:28 -0300

    feat(ISD-844): Add Traefik integration

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -8,8 +8,14 @@ import typing
 
 import requests
 from juju.application import Application
+from juju.model import Model
+from ops.model import ActiveStatus
+from pytest_operator.plugin import OpsTest
 
 from constants import SYNAPSE_PORT
+
+# caused by pytest fixtures
+# pylint: disable=too-many-arguments
 
 logger = logging.getLogger(__name__)
 
@@ -27,3 +33,33 @@ async def test_synapse_is_up(
         response = requests.get(f"http://{unit_ip}:{SYNAPSE_PORT}/_matrix/static/", timeout=5)
         assert response.status_code == 200
         assert "Welcome to the Matrix" in response.text
+
+
+async def test_with_ingress(
+    ops_test: OpsTest,
+    model: Model,
+    synapse_app: Application,
+    traefik_app,  # pylint: disable=unused-argument
+    traefik_app_name: str,
+    external_hostname: str,
+    get_unit_ips: typing.Callable[[str], typing.Awaitable[tuple[str, ...]]],
+):
+    """
+    arrange: build and deploy the Synapse charm, and deploy the Traefik.
+    act: relate the Traefik charm with the Synapse charm.
+    assert: requesting the charm through Traefik should return a correct response.
+    """
+    await model.add_relation(synapse_app.name, traefik_app_name)
+    # mypy doesn't see that ActiveStatus has a name
+    await model.wait_for_idle(status=ActiveStatus.name)  # type: ignore
+
+    traefik_ip = (await get_unit_ips(traefik_app_name))[0]
+    response = requests.get(
+        f"http://{traefik_ip}",
+        headers={
+            "Host": f"{ops_test.model_name}-{synapse_app.name}.{external_hostname}/_matrix/static/"
+        },
+        timeout=5,
+    )
+    assert response.status_code == 200
+    assert "Welcome to the Matrix" in response.text
