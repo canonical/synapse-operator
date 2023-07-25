1d529af465d740b18aea5007d3b42daf4a658167
Amanda H. L. de Andrade Katz <amanda.katz@canonical.com>
2023-07-25 17:43:47 -0300

    Add COS - Prometheus integration (#16)

---
+++ tests/unit/test_synapse_workload.py
@@ -0,0 +1,63 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Synapse workload unit tests."""
+
+# pylint: disable=protected-access
+
+import io
+from unittest.mock import MagicMock, Mock
+
+import ops
+import pytest
+import yaml
+
+import synapse
+from constants import SYNAPSE_CONFIG_PATH
+
+
+def test_enable_metrics_success(monkeypatch: pytest.MonkeyPatch):
+    """
+    arrange: set mock container with file.
+    act: change the configuration file.
+    assert: new configuration file is pushed and metrics are enabled.
+    """
+    config_content = """
+    listeners:
+        - type: http
+          port: 8080
+          bind_addresses:
+            - "::"
+    """
+    text_io_mock = io.StringIO(config_content)
+    pull_mock = Mock(return_value=text_io_mock)
+    push_mock = MagicMock()
+    container_mock = MagicMock()
+    monkeypatch.setattr(container_mock, "pull", pull_mock)
+    monkeypatch.setattr(container_mock, "push", push_mock)
+    synapse.enable_metrics(container_mock)
+    assert pull_mock.call_args[0][0] == SYNAPSE_CONFIG_PATH
+    assert push_mock.call_args[0][0] == SYNAPSE_CONFIG_PATH
+    expected_config_content = {
+        "listeners": [
+            {"type": "http", "port": 8080, "bind_addresses": ["::"]},
+            {"port": 9000, "type": "metrics", "bind_addresses": ["::1", "127.0.0.1"]},
+        ],
+        "enable_metrics": True,
+    }
+    assert push_mock.call_args[0][1] == yaml.safe_dump(expected_config_content)
+
+
+def test_enable_metrics_error(monkeypatch: pytest.MonkeyPatch):
+    """
+    arrange: set mock container with file.
+    act: change the configuration file.
+    assert: raise WorkloadError in case of error.
+    """
+    error_message = "Error pulling file"
+    path_error = ops.pebble.PathError(kind="fake", message=error_message)
+    pull_mock = MagicMock(side_effect=path_error)
+    container_mock = MagicMock()
+    monkeypatch.setattr(container_mock, "pull", pull_mock)
+    with pytest.raises(synapse.WorkloadError, match=error_message):
+        synapse.enable_metrics(container_mock)
