08152d675ca909c4ba6cead795217cf63fee172f
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2023-08-25 17:57:34 -0300

    Fix tests

--- synapse_rock/attributemaps/u1staging.py
+++ synapse_rock/attributemaps/u1staging.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
 MAP = {
     "identifier": "urn:oasis:names:tc:SAML:2.0:attrname-format:unspecified",
     "fro": {
