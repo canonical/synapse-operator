d21edfd6c95d2cf47d4b775cbc29dcea5c2e68f0
Thanhphan1147 <trung.thanh.phan@canonical.com>
2023-09-13 10:21:29 +0200

    refactor unit test : update test_enable_metrics_success

--- tests/unit/test_synapse_workload.py
+++ tests/unit/test_synapse_workload.py
@@ -18,11 +18,11 @@ from charm import SynapseCharm
 from constants import SYNAPSE_CONFIG_PATH, SYNAPSE_CONTAINER_NAME, TEST_SERVER_NAME
 
 
-def test_enable_metrics_success(monkeypatch: pytest.MonkeyPatch):
+def test_enable_metrics_success():
     """
-    arrange: set mock container with file.
+    arrange: Write config to container.
     act: change the configuration file.
-    assert: new configuration file is pushed and metrics are enabled.
+    assert: new configuration file is present and metrics are enabled.
     """
     config_content = """
     listeners:
@@ -31,17 +31,23 @@ def test_enable_metrics_success(monkeypatch: pytest.MonkeyPatch):
           bind_addresses:
             - "::"
     """
-    text_io_mock = io.StringIO(config_content)
-    pull_mock = Mock(return_value=text_io_mock)
-    push_mock = MagicMock()
-    container_mock = MagicMock()
-    monkeypatch.setattr(container_mock, "pull", pull_mock)
-    monkeypatch.setattr(container_mock, "push", push_mock)
 
-    synapse.enable_metrics(container_mock)
+    # Setup charm
+    harness = Harness(SynapseCharm)
+    harness.update_config({"server_name": TEST_SERVER_NAME, "public_baseurl": TEST_SERVER_NAME})
+    harness.set_can_connect(SYNAPSE_CONTAINER_NAME, True)
+    harness.begin()
 
-    assert pull_mock.call_args[0][0] == SYNAPSE_CONFIG_PATH
-    assert push_mock.call_args[0][0] == SYNAPSE_CONFIG_PATH
+    # write config
+    root = harness.get_filesystem_root(SYNAPSE_CONTAINER_NAME)
+    config_path = root / SYNAPSE_CONFIG_PATH[1:]
+    config_path.parent.mkdir(parents=True)
+    config_path.write_text(config_content)
+
+    # Enable metrics in container
+    synapse_container = harness.model.unit.get_container(SYNAPSE_CONTAINER_NAME)
+    synapse.enable_metrics(synapse_container)
+
     expected_config_content = {
         "listeners": [
             {"type": "http", "port": 8080, "bind_addresses": ["::"]},
@@ -49,7 +55,8 @@ def test_enable_metrics_success(monkeypatch: pytest.MonkeyPatch):
         ],
         "enable_metrics": True,
     }
-    assert push_mock.call_args[0][1] == yaml.safe_dump(expected_config_content)
+
+    assert config_path.read_text() == yaml.safe_dump(expected_config_content)
 
 
 def test_enable_metrics_error(monkeypatch: pytest.MonkeyPatch):
