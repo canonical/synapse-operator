d0be9f90eae8c8794c6dc89a7555321c667c5475
Thanhphan1147 <trung.thanh.phan@canonical.com>
2023-09-17 12:47:02 +0200

    fix lint error

--- tests/constants.py
+++ tests/constants.py
@@ -1,7 +1,7 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-""" This module defines Test-related constants """
+"""This module defines Test-related constants"""
 
 TEST_SERVER_NAME = "server-name-configured.synapse.com"
 TEST_SERVER_NAME_CHANGED = "pebble-layer-1.synapse.com"
