fef7f3e735c866c12765a5beed30912ea0978f17
Niels Robin-Aubertin <git@niels.fr>
2024-01-25 10:47:04 -0500

    Fix unit tests

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -38,6 +38,8 @@ def test_synapse_pebble_layer(harness: Harness) -> None:
         "summary": "Synapse application service",
         "command": synapse.SYNAPSE_COMMAND_PATH,
         "environment": {
+            "SYNAPSE_CONFIG_DIR": synapse.SYNAPSE_CONFIG_DIR,
+            "SYNAPSE_CONFIG_PATH": synapse.SYNAPSE_CONFIG_PATH,
             "SYNAPSE_NO_TLS": "True",
             "SYNAPSE_REPORT_STATS": "no",
             "SYNAPSE_SERVER_NAME": TEST_SERVER_NAME,
