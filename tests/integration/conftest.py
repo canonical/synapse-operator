bbd8e110deb841baf3e7832baed6d924428142cb
Mariyan Dimitrov <mariyan.dimitrov@canonical.com>
2024-02-05 16:25:48 +0200

    tests(irc-bridge): Add irc-bridge db

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -249,6 +249,22 @@ async def postgresql_app_fixture(
         await model.wait_for_idle(status=ACTIVE_STATUS_NAME)
 
 
+@pytest.fixture(scope="module", name="irc_postgresql_app_name")
+def irc_postgresql_app_name_app_name_fixture() -> str:
+    """Return the name of the postgresql application deployed for irc bridge tests."""
+    return "irc-postgresql-k8s"
+
+
+@pytest_asyncio.fixture(scope="module", name="irc_postgresql_app")
+async def postgresql_app_fixture(
+    ops_test: OpsTest, model: Model, irc_postgresql_app_name: str, pytestconfig: Config
+):
+    """Deploy postgresql."""
+    async with ops_test.fast_forward():
+        await model.deploy(irc_postgresql_app_name, channel="14/stable", trust=True)
+        await model.wait_for_idle(status=ACTIVE_STATUS_NAME)
+
+
 @pytest.fixture(scope="module", name="grafana_app_name")
 def grafana_app_name_fixture() -> str:
     """Return the name of the grafana application deployed for tests."""
--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -587,6 +587,7 @@ async def test_synapse_enable_s3_backup_integration_no_bucket(
 async def test_synapse_irc_bridge_is_up(
     ops_test: OpsTest,
     synapse_app: Application,
+    irc_postgresql_app: Application,
     get_unit_ips: typing.Callable[[str], typing.Awaitable[tuple[str, ...]]],
 ):
     """
@@ -594,6 +595,8 @@ async def test_synapse_irc_bridge_is_up(
     act: Enable the IRC bridge.
     assert: Synapse and IRC bridge health points should return correct responses.
     """
+    await model.add_relation(irc_postgresql_app.name, f"{synapse_app.name}:irc-bridge-databasi")
+    await model.wait_for_idle(apps=[irc_postgresql_app.name], status=ACTIVE_STATUS_NAME)
     await synapse_app.set_config({"enable_irc_bridge": "true"})
     await synapse_app.model.wait_for_idle(
         idle_period=30, timeout=120, apps=[synapse_app.name], status="blocked"
