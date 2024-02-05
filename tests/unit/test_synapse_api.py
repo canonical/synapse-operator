5dc0d5e3bd260d50441a490c2c82d55ae5c9ae5c
Mariyan Dimitrov <mariyan.dimitrov@canonical.com>
2024-02-05 14:24:31 +0200

    feat(irc-bridge): Add irc database (#166)
    * feat(irc-bridge): Add irc database
    
    * feat(irc-bridge): Add irc database state
    
    * feat(irc-bridge): Add irc config handling (#173)
    
    * feat(irc-bridge): Add irc config handling
    
    * feat(irc-bridge): Add irc config template
    
    * chore(lint): Change example ban site name
    
    * tests(integration): Add irc bridge test
    
    * lint(newline): Add newline

--- tests/unit/test_synapse_api.py
+++ tests/unit/test_synapse_api.py
@@ -223,6 +223,7 @@ def test_override_rate_limit_success(monkeypatch: pytest.MonkeyPatch):
     charm_state = CharmState(
         synapse_config=synapse_config,
         datasource=None,
+        irc_bridge_datasource=None,
         saml_config=None,
         smtp_config=None,
     )
@@ -256,6 +257,7 @@ def test_override_rate_limit_error(monkeypatch: pytest.MonkeyPatch):
     charm_state = CharmState(
         synapse_config=synapse_config,
         datasource=None,
+        irc_bridge_datasource=None,
         saml_config=None,
         smtp_config=None,
     )
