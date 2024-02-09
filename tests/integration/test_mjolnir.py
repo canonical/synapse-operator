9a9b0fdfeadcd07003df4c2a8d36d9e9792c20b7
Niels Robin-Aubertin <git@niels.fr>
2024-02-09 16:17:04 -0500

    Fix

--- tests/integration/test_mjolnir.py
+++ tests/integration/test_mjolnir.py
@@ -19,21 +19,6 @@ from tests.integration.helpers import create_moderators_room, get_access_token, 
 # caused by pytest fixtures
 # pylint: disable=too-many-arguments
 
-async def test_synapse_is_up(
-    synapse_app: Application,
-    get_unit_ips: typing.Callable[[str], typing.Awaitable[tuple[str, ...]]],
-):
-    """
-    arrange: build and deploy the Synapse charm.
-    act: send a request to the Synapse application managed by the Synapse charm.
-    assert: the Synapse application should return a correct response.
-    """
-    for unit_ip in await get_unit_ips(synapse_app.name):
-        response = requests.get(
-            f"http://{unit_ip}:{synapse.SYNAPSE_NGINX_PORT}/_matrix/static/", timeout=5
-        )
-        assert response.status_code == 200
-        assert "Welcome to the Matrix" in response.text
 
 @pytest.mark.mjolnir
 async def test_synapse_with_mjolnir_from_refresh_is_up(
