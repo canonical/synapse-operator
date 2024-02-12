f3f1a97149574a2e44634a8f430fc3cae1bc9b71
Javier de la Puente <javier.delapuente@canonical.com>
2024-02-12 13:58:33 +0100

    Reword exception message to make it concrete

--- src/backup.py
+++ src/backup.py
@@ -210,8 +210,8 @@ class S3Client:
         except ClientError as exc:
             if "Error" in exc.response and exc.response["Error"].get("Code") == "404":
                 return False
-            logger.exception("Error checking object")
-            raise S3Error("Error checking object in S3") from exc
+            logger.exception("Error when checking if S3 object exists with head_object.")
+            raise S3Error("Error when checking if S3 object exists with head_object.") from exc
 
         return True
 
