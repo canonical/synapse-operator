73190b771f0256edcec3935cca71d7d31e662468
Mariyan Dimitrov <mariyan.dimitrov@canonical.com>
2024-02-25 13:51:05 +0200

    tests(exceptions): Add exceptions test

---
+++ tests/unit/test_exceptions.py
@@ -0,0 +1,11 @@
+from exceptions import CharmDatabaseRelationNotFoundError
+
+def test_charm_database_relation_not_found_error():
+    """
+    arrange: set error message.
+    act: create CharmDatabaseRelationNotFoundError instance.
+    assert: error message is set correctly.
+    """
+    error_message = "Database relation not found"
+    error = CharmDatabaseRelationNotFoundError(error_message)
+    assert error.msg == error_message
