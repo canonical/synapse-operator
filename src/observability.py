b08ae5949f319e0d1882dbc401cde391ddfbd8d8
Phan Trung Thanh <trung.thanh.phan@canonical.com>
2025-01-05 22:48:12 +0100

    update licence

--- src/observability.py
+++ src/observability.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Canonical Ltd.
+# Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 """Provide the Observability class to represent the observability stack for Synapse."""
