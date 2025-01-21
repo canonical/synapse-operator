eb3a4d3046cab5a2d1d6be8d95450ef5485d2f2e
Amanda H. L. de Andrade Katz <amanda.katz@canonical.com>
2025-01-21 13:28:10 -0300

    Integrate with Loki and add alert rules based on logs (#662)

--- src/observability.py
+++ src/observability.py
@@ -6,13 +6,12 @@
 
 import ops
 from charms.grafana_k8s.v0.grafana_dashboard import GrafanaDashboardProvider
-from charms.loki_k8s.v1.loki_push_api import LogProxyConsumer
+from charms.loki_k8s.v1.loki_push_api import LogForwarder
 from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
 
 import synapse
 
 CONTAINER_NAME = "synapse"
-LOG_PATHS = ["/debug.log*", "/errors.log*"]
 
 
 class Observability:  # pylint: disable=too-few-public-methods
@@ -47,12 +46,4 @@ class Observability:  # pylint: disable=too-few-public-methods
                 },
             ],
         )
-        self._logging = LogProxyConsumer(
-            charm,
-            relation_name="logging",
-            logs_scheme={
-                f"{CONTAINER_NAME}": {
-                    "log-files": LOG_PATHS,
-                },
-            },
-        )
+        self._logging = LogForwarder(charm, relation_name="logging")
