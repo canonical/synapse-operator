e5964643c14bc2a7764db2d2812719ad8d5a88ec
Amanda H. L. de Andrade Katz <amanda.katz@canonical.com>
2025-03-20 14:57:37 -0300

    Add moderation service (#714)

--- src/state/charm_state.py
+++ src/state/charm_state.py
@@ -62,6 +62,8 @@ class SynapseConfig(BaseModel):
         invite_checker_policy_rooms: invite_checker_policy_rooms config.
         ip_range_whitelist: ip_range_whitelist config.
         limit_remote_rooms_complexity: limit_remote_rooms_complexity config.
+        moderation_access_token_secret_id: moderation_access_token_secret_id config.
+        moderation_room_alias: moderation_room_alias config.
         notif_from: defines the "From" address to use when sending emails.
         public_baseurl: public_baseurl config.
         publish_rooms_allowlist: publish_rooms_allowlist config.
@@ -85,6 +87,8 @@ class SynapseConfig(BaseModel):
     invite_checker_policy_rooms: str | None = Field(None)
     ip_range_whitelist: str | None = Field(None, regex=r"^[\.:,/\d]+\d+(?:,[:,\d]+)*$")
     limit_remote_rooms_complexity: float | None = Field(None)
+    moderation_access_token_secret_id: str | None = Field(None)
+    moderation_room_alias: str | None = Field(None)
     public_baseurl: str = Field(..., min_length=2)
     publish_rooms_allowlist: str | None = Field(None)
     rc_joins_remote_burst_count: int | None = Field(None)
@@ -232,7 +236,7 @@ class SynapseConfig(BaseModel):
 
 
 @dataclasses.dataclass(frozen=True)
-class CharmState:
+class CharmState:  # pylint: disable=too-many-instance-attributes
     """State of the Charm.
 
     Attributes:
@@ -244,6 +248,8 @@ class CharmState:
         proxy: proxy information.
         instance_map_config: Instance map configuration with main and worker addresses.
         registration_secrets: Registration secrets received via matrix-auth integration.
+        moderation_token: Token extracted from secret id provided via
+            moderation_access_token_secret_id.
     """
 
     synapse_config: SynapseConfig
@@ -253,6 +259,7 @@ class CharmState:
     redis_config: typing.Optional[RedisConfiguration]
     instance_map_config: typing.Optional[typing.Dict]
     registration_secrets: typing.Optional[typing.List]
+    moderation_token: typing.Optional[str] = None
 
     @property
     def proxy(self) -> "ProxyConfig":
@@ -333,6 +340,10 @@ class CharmState:
                         logger.warning(
                             "Worker %s in workers_ignore_list not found in instance_map", worker
                         )
+            # get secret content from moderation_access_token_secret_id
+            moderation_token = get_moderation_token(
+                charm, valid_synapse_config.moderation_access_token_secret_id
+            )
         except ValidationError as exc:
             error_fields = set(
                 itertools.chain.from_iterable(error["loc"] for error in exc.errors())
@@ -347,4 +358,32 @@ class CharmState:
             redis_config=redis_config,
             instance_map_config=instance_map_config,
             registration_secrets=registration_secrets,
+            moderation_token=moderation_token,
         )
+
+
+def get_moderation_token(
+    charm: ops.CharmBase, secret_id: typing.Optional[str]
+) -> typing.Optional[str]:
+    """Retrieve the moderation access token from a secret.
+
+    Args:
+        charm (ops.CharmBase): The charm instance, used to access the model secrets.
+        secret_id (Optional[str]): The ID of the secret storing the moderation token.
+
+    Returns:
+        Optional[str]: The moderation access token if found, otherwise None.
+    """
+    if not secret_id:
+        return None
+
+    try:
+        moderation_secret: ops.Secret = charm.model.get_secret(id=secret_id)
+        moderation_secret_content = moderation_secret.get_content()
+        return moderation_secret_content.get("matrix-access-token", None)
+    except ops.SecretNotFoundError:
+        logger.warning("Moderation secret id %s not found", secret_id)
+    except ops.ModelError as e:
+        logger.warning("Unable to access the secret: %s", e)
+
+    return None
--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -269,3 +269,64 @@ async def test_nginx_route_integration(
     )
     assert response.status_code == 200
     assert "Welcome to the Matrix" in response.text
+
+
+async def test_moderation(
+    model: Model,
+    synapse_app: Application,
+    get_unit_ips: typing.Callable[[str], typing.Awaitable[tuple[str, ...]]],
+):
+    """
+    arrange: deploy the charm, create user and moderation room and create the
+        moderation secret.
+    act: set moderation_access_token_secret_id
+    assert: the Draupnir health endpoint should return OK.
+    """
+    # create user
+    synapse_unit: Unit = next(iter(synapse_app.units))
+    register_user_action: Action = await synapse_unit.run_action(
+        "register-user", username="moderator", admin=True
+    )
+    await register_user_action.wait()
+    assert register_user_action.status == "completed"
+    assert register_user_action.results["user-password"]
+    password = register_user_action.results["user-password"]
+    # get token
+    synapse_ip = (await get_unit_ips(synapse_app.name))[0]
+    url = f"http://{synapse_ip}:8080/_matrix/client/r0/login"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "type": "m.login.password",
+        "identifier": {"type": "m.id.user", "user": "moderator"},
+        "password": f"{password}",
+    }
+    response = requests.post(url, json=data, headers=headers, timeout=10)
+    assert response.status_code == 200
+    access_token = response.json().get("access_token")
+    assert access_token
+    # create room
+    url = f"http://{synapse_ip}:8080/_matrix/client/v3/createRoom"
+    headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
+    data = {"room_alias_name": "moderators", "name": "moderators", "visibility": "private"}
+    response = requests.post(url, json=data, headers=headers, timeout=10)
+    assert response.status_code == 200
+    room_id = response.json().get("room_id")
+    assert room_id
+    # create secret
+    # refers to juju secret name, not hardcoded password.
+    secret = await model.add_secret(
+        "moderation", data_args=[f"matrix-access-token={access_token}"]
+    )
+    secret_id = secret.split(":")[-1]
+    await model.grant_secret("moderation", synapse_app.name)
+
+    # change synapse configuration
+    await synapse_app.set_config({"moderation_access_token_secret_id": secret_id})
+    await synapse_app.model.wait_for_idle(
+        idle_period=30, timeout=120, apps=[synapse_app.name], status="active"
+    )
+
+    # verify draupnir health endpoint
+    response = requests.get(f"http://{synapse_ip}:7777/", timeout=5)
+    assert response.status_code == 200
+    assert "health code: 200" in response.text
