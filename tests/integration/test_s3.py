28200fcc59c88ca35c4d63ad8d38f4c7a4f90f24
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2025-03-28 16:20:47 -0300

    Add test_synapse_create_backup_correct_media_sync_cleanup test

--- tests/integration/test_s3.py
+++ tests/integration/test_s3.py
@@ -297,3 +297,58 @@ async def test_synapse_enable_media(  # pylint: disable=too-many-positional-argu
     key = f"/medialocal_content/{media_id[:2]}/{media_id[2:4]}/{media_id[4:]}"
     s3objresp = boto_s3_media_client.get_object(Bucket=bucket_name, Key=key)
     assert s3objresp["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+
+@pytest.mark.s3
+@pytest.mark.usefixtures("s3_backup_bucket")
+@pytest.mark.usefixtures("s3_media_bucket")
+async def test_synapse_create_backup_correct_media_sync_cleanup(  # noqa: E501 pylint: disable=line-too-long, too-many-positional-arguments
+    model: Model,
+    synapse_app: Application,
+    s3_integrator_app_backup: Application,
+    s3_backup_configuration: dict,
+    boto_s3_client: typing.Any,
+    s3_integrator_app_media: Application,
+):
+    """
+    arrange: Synapse App deployed and related with s3-integrator together with media.
+        enable_media_sync_cleanup and backup_passphrase set.
+    act: Run create-backup action
+    assert: Correct response from the action that includes the backup-id.
+       An encrypted object was created in S3 with the correct name.
+    """
+    await model.add_relation(f"{s3_integrator_app_media.name}", f"{synapse_app.name}:media")
+    await model.wait_for_idle(
+        idle_period=30,
+        apps=[synapse_app.name, s3_integrator_app_media.name],
+        status=ACTIVE_STATUS_NAME,
+    )
+
+    await model.add_relation(s3_integrator_app_backup.name, f"{synapse_app.name}:backup")
+    passphrase = token_hex(16)
+    await synapse_app.set_config(
+        {"backup_passphrase": passphrase, "enable_media_sync_cleanup": True}
+    )
+    await model.wait_for_idle(
+        idle_period=30,
+        apps=[synapse_app.name, s3_integrator_app_backup.name],
+        status=ACTIVE_STATUS_NAME,
+    )
+
+    synapse_unit: Unit = next(iter(synapse_app.units))
+    backup_action: Action = await synapse_unit.run_action("create-backup")
+    await backup_action.wait()
+
+    assert backup_action.status == "completed"
+    assert backup_action.results["media-sync-cleanup-result"] == "correct"
+    assert "backup-id" in backup_action.results
+    bucket_name = s3_backup_configuration["bucket"]
+    path = s3_backup_configuration["path"].strip("/")
+    object_key = f"{path}/{backup_action.results['backup-id']}"
+    s3objresp = boto_s3_client.get_object(Bucket=bucket_name, Key=object_key)
+    objbuf = s3objresp["Body"].read()
+    # GnuPG 2.2.x and earlier outputs "GPG symmetrically encrypted data (AES256 cipher)"
+    assert (
+        "PGP symmetric key encrypted data - AES with 256-bit key salted & iterated - SHA512"
+        in magic.from_buffer(objbuf)
+    )
