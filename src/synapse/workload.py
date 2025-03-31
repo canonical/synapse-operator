c21b8cb75965a366a393916b39c6229f7178d113
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2025-03-31 17:17:07 -0300

    Create patch for s3_media_upload

--- src/synapse/workload.py
+++ src/synapse/workload.py
@@ -406,12 +406,6 @@ def run_media_sync_cleanup(container: ops.Container, charm_state: CharmState) ->
     s3_media_upload_path = "/usr/local/bin/s3_media_upload"
     commands = [
         [
-            "sed",
-            "-i",
-            "s|#!/root/parts/synapse/install/usr/bin/python3|#!/usr/bin/python3|",
-            "/usr/local/bin/s3_media_upload",
-        ],
-        [
             s3_media_upload_path,
             "--no-progress",
             "update",
