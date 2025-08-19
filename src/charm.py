98902f6cdc58730d736c7361b9ed9fd8fe4f59ff
Amanda Hager Lopes de Andrade Katz <amanda.katz@canonical.com>
2025-08-19 17:37:36 -0300

    Update version

--- src/charm.py
+++ src/charm.py
@@ -46,7 +46,7 @@ logger = logging.getLogger(__name__)
 
 INGRESS_INTEGRATION_NAME = "ingress"
 # This constant is updated by Renovate.
-SYNAPSE_VERSION = "1.134.0"
+SYNAPSE_VERSION = "1.136.0"
 
 
 class SynapseCharm(CharmBaseWithState):
