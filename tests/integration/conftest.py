fd2982d8ef91138fe8f56997ce6b9a6340a10404
Niels Robin-Aubertin <git@niels.fr>
2025-10-15 18:51:15 -0400

    Fix

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -130,11 +130,6 @@ async def synapse_app_fixture(
 
     async with ops_test.fast_forward():
         await model.relate(f"{synapse_app_name}:database", f"{postgresql_app.name}")
-        await model.wait_for_idle(
-            apps=[synapse_app_name, postgresql_app.name],
-            status=ACTIVE_STATUS_NAME,
-            idle_period=5,
-        )
         await model.relate(f"{synapse_app_name}:mas-database", f"{postgresql_app.name}:database")
         await model.wait_for_idle(
             apps=[synapse_app_name, postgresql_app.name],
