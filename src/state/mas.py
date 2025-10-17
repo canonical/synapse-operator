066f882989b1b5e31e0f0d86268d3a5345770e5d
Niels Robin-Aubertin <git@niels.fr>
2025-10-17 15:36:14 -0400

    Remove debug

--- src/state/mas.py
+++ src/state/mas.py
@@ -222,4 +222,4 @@ def validate_datasource(datasource: DatasourcePostgreSQL) -> None:
             datasource.get("port"),
         ]
     ):
-        raise MASDatasourceInvalidError(f"Missing values in postgresql datasource: {datasource}")
+        raise MASDatasourceInvalidError("Missing values in postgresql datasource.")
