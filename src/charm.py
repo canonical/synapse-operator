efa2a884d201814270ebada81fc9a7555c6557b7
Amanda H. L. de Andrade Katz <amanda.katz@canonical.com>
2025-12-15 10:56:22 -0300

    Update SYNAPSE_VERSION format

--- src/charm.py
+++ src/charm.py
@@ -64,7 +64,7 @@ logger = logging.getLogger(__name__)
 
 INGRESS_INTEGRATION_NAME = "ingress"
 # This constant is updated by Renovate.
-SYNAPSE_VERSION = "v1.144.0"
+SYNAPSE_VERSION = "1.144.0"
 S3_CANNOT_ACCESS_BUCKET = "Media: S3 bucket does not exist or cannot be accessed"
 S3_INVALID_CONFIGURATION = "Media: S3 configuration is invalid"
 
