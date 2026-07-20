1d72f54dfe40915af8310fd7e847cb8900f4b2ec
erinecon <erin.conley@canonical.com>
2026-07-20 14:52:41 -0400

    fix(docs): update intersphinx defs and targets

--- docs/conf.py
+++ docs/conf.py
@@ -390,9 +390,8 @@ if os.path.exists('./reuse/substitutions.yaml'):
 # Add configuration for intersphinx mapping
 # Map only the Sphinx documentation sets that you need to link to from your docs set.
 intersphinx_mapping = {
-    'starter-pack': ('https://canonical-starter-pack.readthedocs-hosted.com/stable/', None),
-    'juju': ('https://documentation.ubuntu.com/juju/3.6/', None),
-    'traefik-k8s-charm': ('https://documentation.ubuntu.com/traefik-k8s-charm/latest/', None),
-    'postgresql-k8s-charm': ('https://canonical-charmed-postgresql-k8s.readthedocs-hosted.com/14/', None),
-    'postgresql-charm': ('https://canonical-charmed-postgresql.readthedocs-hosted.com/14/', None),
+    'starter-pack': ('https://documentation.ubuntu.com/sphinx-stack/latest/', None),
+    'juju': ('https://canonical.com/juju/docs/juju-cli/3.6/', None),
+    'traefik-k8s-charm': ('https://canonical.com/juju/docs/traefik-k8s-charm/latest/', None),
+    'postgresql-charms': ('https://canonical.com/data/postgresql/docs/16/', None),
 }
