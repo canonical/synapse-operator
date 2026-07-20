729ee496211e0babee5b1695519c483a56b6015b
erinecon <erin.conley@canonical.com>
2026-07-20 14:42:08 -0400

    chore(docs): add sitemap_filename var

--- docs/conf.py
+++ docs/conf.py
@@ -196,6 +196,8 @@ html_baseurl = f"https://canonical.com/juju/docs/synapse-charm/{version}/"
 
 sitemap_url_scheme = '{link}'
 
+sitemap_filename = "doc-sitemap.xml"
+
 # Include `lastmod` dates in the sitemap:
 
 sitemap_show_lastmod = True
